ada8c2e9c610ea99c242cab23166fcf38667bd17
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-09-05 17:18:32 -0700

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,10 @@
+//Author: Drew Cornatzer
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
+    std::cout << "Hi, My name is Drew." << std::endl;
     std::cout << "What's your first name?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
